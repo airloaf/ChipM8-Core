f9a9a40cd2f8f0bb163266e34dd72950682cf3b2
Vikram Singh <vikzilla18@gmail.com>
2020-01-15 23:47:38 -0500

    Created interpreter initialization tests

--- tests/InterpreterInitialization.cpp
+++ tests/InterpreterInitialization.cpp
@@ -32,17 +32,123 @@ BOOST_AUTO_TEST_CASE(TestRegisterInitialValues){
     BOOST_TEST(interpreter.registers.DT == 0);
 }
 
-static auto HexDigitValues = bdata::make({
-        0xF0, 0xF0 
-        });
-
 /**
- * Checks that the Hexadecimal Digtis are set up correctly in memory.
+ * Checks that the 0 hex digit is properly set in memory
  **/
-BOOST_AUTO_TEST_CASE(HexDigits){
+BOOST_AUTO_TEST_CASE(HexDigits0){
     // Create an interpreter
     Interpreter interpreter;
 
+    uint16_t addressOffset = 0x00;
+
+    // Check that 0 is stored in memory properly
+    BOOST_TEST(interpreter.memory[0x00 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x01 + addressOffset] == 0x90);
+    BOOST_TEST(interpreter.memory[0x02 + addressOffset] == 0x90);
+    BOOST_TEST(interpreter.memory[0x03 + addressOffset] == 0x90);
+    BOOST_TEST(interpreter.memory[0x04 + addressOffset] == 0xF0);
+}
+
+/**
+ * Checks that the 1 hex digit is properly set in memory
+ **/
+BOOST_AUTO_TEST_CASE(HexDigits1){
+    // Create an interpreter
+    Interpreter interpreter;
+
+    uint16_t addressOffset = 0x05;
+
+    // Check that 0 is stored in memory properly
+    BOOST_TEST(interpreter.memory[0x00 + addressOffset] == 0x20);
+    BOOST_TEST(interpreter.memory[0x01 + addressOffset] == 0x60);
+    BOOST_TEST(interpreter.memory[0x02 + addressOffset] == 0x20);
+    BOOST_TEST(interpreter.memory[0x03 + addressOffset] == 0x20);
+    BOOST_TEST(interpreter.memory[0x04 + addressOffset] == 0x70);
+}
+
+/**
+ * Checks that the 2 hex digit is properly set in memory
+ **/
+BOOST_AUTO_TEST_CASE(HexDigits2){
+    // Create an interpreter
+    Interpreter interpreter;
+
+    uint16_t addressOffset = 0x0A;
+
+    // Check that 0 is stored in memory properly
+    BOOST_TEST(interpreter.memory[0x00 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x01 + addressOffset] == 0x10);
+    BOOST_TEST(interpreter.memory[0x02 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x03 + addressOffset] == 0x80);
+    BOOST_TEST(interpreter.memory[0x04 + addressOffset] == 0xF0);
+}
+
+/**
+ * Checks that the 3 hex digit is properly set in memory
+ **/
+BOOST_AUTO_TEST_CASE(HexDigits3){
+    // Create an interpreter
+    Interpreter interpreter;
+
+    uint16_t addressOffset = 0x0F;
+
+    // Check that 0 is stored in memory properly
+    BOOST_TEST(interpreter.memory[0x00 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x01 + addressOffset] == 0x10);
+    BOOST_TEST(interpreter.memory[0x02 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x03 + addressOffset] == 0x10);
+    BOOST_TEST(interpreter.memory[0x04 + addressOffset] == 0xF0);
+}
+
+/**
+ * Checks that the 4 hex digit is properly set in memory
+ **/
+BOOST_AUTO_TEST_CASE(HexDigits4){
+    // Create an interpreter
+    Interpreter interpreter;
+
+    uint16_t addressOffset = 0x14;
+
+    // Check that 0 is stored in memory properly
+    BOOST_TEST(interpreter.memory[0x00 + addressOffset] == 0x90);
+    BOOST_TEST(interpreter.memory[0x01 + addressOffset] == 0x90);
+    BOOST_TEST(interpreter.memory[0x02 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x03 + addressOffset] == 0x10);
+    BOOST_TEST(interpreter.memory[0x04 + addressOffset] == 0x10);
+}
+
+/**
+ * Checks that the 5 hex digit is properly set in memory
+ **/
+BOOST_AUTO_TEST_CASE(HexDigits5){
+    // Create an interpreter
+    Interpreter interpreter;
+
+    uint16_t addressOffset = 0x19;
+
+    // Check that 0 is stored in memory properly
+    BOOST_TEST(interpreter.memory[0x00 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x01 + addressOffset] == 0x80);
+    BOOST_TEST(interpreter.memory[0x02 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x03 + addressOffset] == 0x10);
+    BOOST_TEST(interpreter.memory[0x04 + addressOffset] == 0xF0);
+}
+
+/**
+ * Checks that the 6 hex digit is properly set in memory
+ **/
+BOOST_AUTO_TEST_CASE(HexDigits6){
+    // Create an interpreter
+    Interpreter interpreter;
+
+    uint16_t addressOffset = 0x1E;
+
+    // Check that 0 is stored in memory properly
+    BOOST_TEST(interpreter.memory[0x00 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x01 + addressOffset] == 0x80);
+    BOOST_TEST(interpreter.memory[0x02 + addressOffset] == 0xF0);
+    BOOST_TEST(interpreter.memory[0x03 + addressOffset] == 0x90);
+    BOOST_TEST(interpreter.memory[0x04 + addressOffset] == 0xF0);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
